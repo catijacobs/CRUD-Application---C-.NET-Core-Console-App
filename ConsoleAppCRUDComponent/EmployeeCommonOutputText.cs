f42d03b1b55ffd60276923020ac4ed7da799907b
catiajacob <catia.jacob@inovflow.pt>
2021-05-17 16:20:05 +0100

    add EmployeeCommonOutputText class

---
+++ ConsoleAppCRUDComponent/EmployeeCommonOutputText.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleAppCRUDComponent.Data;
+
+namespace ConsoleAppCRUDComponent
+{
+    public class EmployeeCommonOutputText
+    {
+        public static string GetColumnHeading()
+        {
+            string id = "Id";
+            string firstName = "First Name";
+            string lastName = "Last Name";
+            string annualSalary = "Annual Salary";
+            string gender = "Gender";
+            string manager = "Manager";
+            string heading = $"{id.ToString().PadRight(6)}\t{firstName.PadRight(15)}\t{lastName.PadRight(15)}\t{annualSalary.ToString().PadRight(15)}\t{gender.ToString().PadRight(6)}\t{manager.ToString().PadRight(7)}\n";
+            heading += $"{new string('_', 6)}\t{new string('_', 15)}\t{new string('_', 15)}\t{new string('_', 15)}\t{new string('_', 6)}\t{new string('_', 7)}\n";
+        }
+        return GetColumnHeading;
+    }
+}
---
+++ ConsoleAppCRUDComponent/Views/EmployeeRecordsView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleAppCRUDComponent.Data;
+
+namespace ConsoleAppCRUDComponent.Views
+{
+    public class EmployeeRecordsView
+    {
+        private Employees _employees = null;
+
+        public EmployeeRecordsView(Employees employees)
+        {
+            _employees = employees;
+        }
+
+        public void RunRecordsView()
+        {
+            foreach(Employee emp in _employees)
+            {
+                Console.Write(emp.GetEmployeeInformation());
+            }
+        }
+    }
+}
