8f2745c8067acbdae35b8ac5b3007c14a5f82068
catiajacob <catia.jacob@inovflow.pt>
2021-05-17 15:44:42 +0100

    EmployeeObjectFactory set class

---
+++ ConsoleAppCRUDComponent/Data/Employees.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppCRUDComponent.Data
+{
+    public class Employees
+    {
+        ArrayList _employeeList = null;
+
+        public Employees()
+        {
+            _employeeList = new ArrayList();
+        }
+
+        private void SeedData()
+        {
+            this.Add();
+        }
+
+        public void Add(Employee employee)
+        {
+            _employeeList.Add(employee);
+        }
+    }
+}
---
+++ ConsoleAppCRUDComponent/EmployeeObjectFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleAppCRUDComponent.Data;
+
+namespace ConsoleAppCRUDComponent
+{
+    public static class EmployeeObjectFactory
+    {
+        public static Employee CreateNewEmployeeObject(string firstName, string lastName, decimal annualSalary, char gender, bool isManager)
+        {
+            return new Employee(firstName, lastName, annualSalary, gender, isManager);
+        }
+    }
+}
