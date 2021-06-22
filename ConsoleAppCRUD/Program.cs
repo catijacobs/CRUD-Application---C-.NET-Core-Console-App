a8d513edaca901f59b7fa24d419c0e13a4a38fc3
catiajacob <catia.jacob@inovflow.pt>
2021-06-22 21:57:27 +0100

    set keys

--- ConsoleAppCRUD/Program.cs
+++ ConsoleAppCRUD/Program.cs
@@ -9,14 +9,55 @@ namespace ConsoleAppCRUD
     {
         static void Main(string[] args)
         {
+            bool blnEnd = false;
+
 
             Employees employees = new Employees();
 
             EmployeeRecordsView employeeRecordsView = EmployeeObjectFactory.EmployeeRecordsViewObject(employees);
 
-            Console.WriteLine(EmployeeCommonOutputText.GetApplicationHeading());
+            while (!blnEnd)
+            {
+                Console.Clear();
 
-            employeeRecordsView.RunRecordsView();
+                Console.WriteLine(EmployeeCommonOutputText.GetApplicationHeading());
+
+                employeeRecordsView.RunRecordsView();
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                ConsoleKey instructionKey = Console.ReadKey().Key;
+
+                switch (instructionKey)
+                {
+                    case ConsoleKey.C:
+                        Console.Clear();
+                        Console.WriteLine("Create funcionality not yet implemented");
+                        break;
+
+                    case ConsoleKey.R:
+                        Console.Clear();
+                        Console.WriteLine("Read funcionality not yet implemented");
+                        break;
+
+                    case ConsoleKey.U:
+                        Console.Clear();
+                        Console.WriteLine("Update funcionality not yet implemented");
+                        break;
+
+                    case ConsoleKey.D:
+                        Console.Clear();
+                        Console.WriteLine("Delete funcionality not yet implemented");
+                        break;
+
+                    default:
+                        blnEnd = true;
+                }
+
+            }
+
+          
 
             Console.ReadKey();
         }
